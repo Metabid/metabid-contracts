4f7112ca4b714f4053b8140d9696cd8e76d0a7c3
Fatih <fatihfurkn@gmail.com>
2022-07-25 19:10:48 +0300

    fix time error

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -5,62 +5,99 @@ import "./IERC721.sol";
 import "./IERC20.sol";
 
 contract MarketPlace {
-  IERC20 immutable METABLOCK;
+    IERC20 immutable METABLOCK;
 
-  event NewBid(address indexed bidder, address indexed nftAddress, uint256 tokenId, uint256 amount, uint256 timestamp);
-  event BidAccepted(address indexed bidder, address indexed nftAddress, uint256 tokenId, uint256 amount, uint256 timestamp);
+    event NewBid(
+        address indexed bidder,
+        address indexed nftAddress,
+        uint256 tokenId,
+        uint256 amount,
+        uint256 timestamp
+    );
+    event BidAccepted(
+        address indexed bidder,
+        address indexed nftAddress,
+        uint256 tokenId,
+        uint256 amount,
+        uint256 timestamp
+    );
 
-  struct Bid {
-    address bidder;
-    uint256 bidTime;
+    struct Bid {
+        address bidder;
+        uint256 bidTime;
+        uint256 amount;
+    }
 
-    uint256 amount;
-  }
+    // nft address => token id => Bid
+    mapping(address => mapping(uint256 => Bid)) public bids;
 
-  // nft address => token id => Bid
-  mapping(address => mapping(uint256 => Bid)) public bids;
+    constructor(address _metaBlockAddress) {
+        METABLOCK = IERC20(_metaBlockAddress);
+    }
 
-  constructor(address _metaBlockAddress) {
-    METABLOCK = IERC20(_metaBlockAddress);
-  }
+    function giveBid(
+        address _nftAddress,
+        uint256 _tokenId,
+        uint256 _amount
+    ) external {
+        Bid memory currentBid = bids[_nftAddress][_tokenId]; // save gas
 
-  function giveBid(address _nftAddress, uint256 _tokenId, uint256 _amount) external {
-    Bid memory currentBid = bids[_nftAddress][_tokenId]; // save gas
+        require(
+            IERC721(_nftAddress).ownerOf(_tokenId) != msg.sender,
+            "cannot bid to self"
+        );
+        require(currentBid.amount < _amount, "not enough amount");
 
-    require(IERC721(_nftAddress).ownerOf(_tokenId) != msg.sender, 'cannot bid to self');
-    require(currentBid.amount < _amount, "not enough amount");
+        // take payment from the new user
+        METABLOCK.transferFrom(msg.sender, address(this), _amount);
 
-    // take payment from the new user
-    METABLOCK.transferFrom(msg.sender, address(this), _amount);
+        // send payment back to latest bidder
+        if (currentBid.bidder != address(0))
+            METABLOCK.transfer(currentBid.bidder, currentBid.amount);
 
-    // send payment back to latest bidder
-    if(currentBid.bidder != address(0))
-      METABLOCK.transfer(currentBid.bidder, currentBid.amount);
+        // save new bid
+        bids[_nftAddress][_tokenId] = Bid({
+            bidder: msg.sender,
+            bidTime: block.timestamp,
+            amount: _amount
+        });
 
-    // save new bid
-    bids[_nftAddress][_tokenId] = Bid({
-      bidder: msg.sender,
-      bidTime: block.timestamp,
-      amount: _amount
-    });
+        emit NewBid(
+            msg.sender,
+            _nftAddress,
+            _tokenId,
+            _amount,
+            block.timestamp
+        );
+    }
 
-    emit NewBid(msg.sender, _nftAddress, _tokenId, _amount, block.timestamp);
-  }
+    function acceptBid(address _nftAddress, uint256 _tokenId) external {
+        Bid memory currentBid = bids[_nftAddress][_tokenId]; // save gas
 
-  function acceptBid(address _nftAddress, uint256 _tokenId) external {
-    Bid memory currentBid = bids[_nftAddress][_tokenId]; // save gas
+        require(
+            currentBid.bidTime + 120 < block.timestamp,
+            "cannot accept yet"
+        );
 
-    require(currentBid.bidTime + 120 > block.timestamp, 'cannot accept yet');
+        // send nft to bidder
+        IERC721(_nftAddress).transferFrom(
+            msg.sender,
+            currentBid.bidder,
+            _tokenId
+        );
 
-    // send nft to bidder
-    IERC721(_nftAddress).transferFrom(msg.sender, currentBid.bidder, _tokenId);
+        // clear bid
+        delete bids[_nftAddress][_tokenId];
 
-    // clear bid
-    delete bids[_nftAddress][_tokenId];
+        // send payment to owner
+        METABLOCK.transfer(msg.sender, currentBid.amount);
 
-    // send payment to owner
-    METABLOCK.transfer(msg.sender, currentBid.amount);
-
-    emit BidAccepted(currentBid.bidder, _nftAddress, _tokenId, currentBid.amount, block.timestamp);
-  }
+        emit BidAccepted(
+            currentBid.bidder,
+            _nftAddress,
+            _tokenId,
+            currentBid.amount,
+            block.timestamp
+        );
+    }
 }
